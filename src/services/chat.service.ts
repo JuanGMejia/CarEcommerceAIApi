3186b0fb9816fb73b967119acffd13005d611f26
Juan Guillermo Mejia Soto <juan.mejias@perficient.com>
2025-07-29 12:20:40 -0500

    Refactor logging and add blob arg
    Add BLOB_NAME argument to Dockerfile for dynamic env setup.
    Enhance chat service error handling using optional chaining and console logging

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -9,21 +9,22 @@ import * as appInsights from 'applicationinsights';
 export class ChatService {
 
   openai = new OpenAI({
-    apiKey: process.env.OPENAI_ID, 
+    apiKey: process.env.OPENAI_ID,
   });
 
   constructor(private readonly embedService: EmbedService) { }
 
   async sendMessageToAgent(message: string): Promise<string> {
-  try {
-      appInsights.defaultClient.trackEvent({ name: "ChatMessageSent", properties: { message } });
-      
+    try {
+      appInsights?.defaultClient?.trackEvent({ name: "ChatMessageSent", properties: { message } });
+
       const response: number[] = await this.embedService.getEmbeddings(message);
       const responseEmbedding = await this.embedService.queryToVectorDB(response);
       return await this.getChatResponse(message, responseEmbedding.join(' '));
     } catch (error) {
-      appInsights.defaultClient.trackException({exception: error, properties: { message } });
-      throw error; 
+      console.error('Error in sendMessageToAgent:', error);
+      appInsights?.defaultClient?.trackException({ exception: error, properties: { message } });
+      throw error;
     }
   }
 
@@ -32,7 +33,8 @@ export class ChatService {
     const response = await this.openai.chat.completions.create({
       model: 'o3-mini',
       "messages": [
-        { "role": "system", "content": `This is the context you need to answer the question of the user ${context}, be concise and polite with the answer, 
+        {
+          "role": "system", "content": `This is the context you need to answer the question of the user ${context}, be concise and polite with the answer, 
                                         if you don't find any information about the question here, tell the user this chat will solve only questions about our Car E-commerce company` },
         { "role": "user", "content": question }
       ],
