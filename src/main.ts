c33c632c5e358282cd1532185db47b9e7dfee2a2
JuanGMejia <JuanGMejia@users.noreply.github.com>
2025-07-29 11:45:34 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200'], // ✅ allow your frontend origin
+    origin: ['http://localhost:4200', 'https://car-ecommerce-api-v-axg5c5fxhfeydqdj.canadacentral-01.azurewebsites.net'], // ✅ allow your frontend origin
     credentials: true, // ✅ allow cookies and Authorization headers
   });
   await app.listen(process.env.PORT ?? 3000);
